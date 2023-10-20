9d36cce912c262288f88e3d82c7bb89ae8efa42c
Anastasia Carlii <nastjaorosszul@gmail.com>
2023-10-20 23:26:55 +0100

    sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 
+import { Section } from './Section/Section';
+import { Statistics } from './Statistics/Statistics';
+
 import css from './App.module.css';
 
 export class App extends Component {
@@ -8,4 +11,40 @@ export class App extends Component {
     neutral: 0,
     bad: 0,
   };
+
+  onLeaveFeedback = option => {
+    this.setState(prevState => ({
+      [option]: prevState[option] + 1,
+    }));
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    const { good } = this.state;
+    return Math.round((good / this.countTotalFeedback()) * 100);
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    // const options = Object.keys(this.state);
+
+    return (
+      <div className={css.container}>
+        <Section title="Please leave feedback"></Section>
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.countPositiveFeedbackPercentage()}
+          />
+        </Section>
+      </div>
+    );
+  }
 }
